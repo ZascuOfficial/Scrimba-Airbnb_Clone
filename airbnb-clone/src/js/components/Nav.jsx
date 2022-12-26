3280da783688de1a1cee573eef9630a7ddc37ea9
Zascu <zascu.github@gmail.com>
2022-12-26 20:54:07 +0200

    Add a class to the link the logo is placed in

--- airbnb-clone/src/js/components/Nav.jsx
+++ airbnb-clone/src/js/components/Nav.jsx
@@ -5,7 +5,11 @@ import airbnbLogo from '../../assets/img/airbnb-logo.png';
 export default function Nav() {
   return (
     <nav className="nav">
-      <a href="https://www.airbnb.com/" target="_blank">
+      <a
+        href="https://www.airbnb.com/"
+        target="_blank"
+        className='airbnb-link'
+      >
         <img
           src={airbnbLogo}
           alt="airbnb-logo"
