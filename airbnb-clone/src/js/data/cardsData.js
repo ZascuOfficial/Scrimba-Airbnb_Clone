77275f40cc2cb0f5c588b657f8e080e6885c3991
Zascu <zascu.github@gmail.com>
2022-12-26 20:15:30 +0200

    Fill in the necessary data for the card elements

---
+++ airbnb-clone/src/js/data/cardsData.js
@@ -0,0 +1,34 @@
+import katieZaferes from '../../assets/cardImg/katie-zaferes.png';
+import weddingPhoto from '../../assets/cardImg/wedding-photo.png';
+import mountainBike from '../../assets/cardImg/mountain-bike.png';
+
+const cardsData = [
+  {
+    img: katieZaferes,
+    title: 'Life Lessons with Katie Zaferes',
+    price: 136,
+    rating: '5.0',
+    reviewsCount: 6,
+    location: 'USA'
+  },
+
+  {
+    img: weddingPhoto,
+    title: 'Learn Wedding Photography',
+    price: 125,
+    rating: '5.0',
+    reviewsCount: 30,
+    location: 'Online'
+  },
+
+  {
+    img: mountainBike,
+    title: 'Group Mountain Biking',
+    price: 50,
+    rating: '4.8',
+    reviewsCount: 2,
+    location: 'Norway'
+  }
+];
+
+export default cardsData;
